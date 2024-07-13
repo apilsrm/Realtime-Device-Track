73e26f33429b4c1a95b13dcacf5e311b627aa5f5
apilsrm <sharmaapil05@gmail.com>
2024-07-13 23:54:16 +0545

    postion+ejs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import { Server } from "socket.io";
  import { createServer } from "http";
 import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 
@@ -11,10 +12,20 @@ const io = new Server(server);
 
 
 app.set("view engine", "ejs");
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename);
 app.set(express.static(path.join(__dirname, "public")));
+// console.log(path.join(__dirname, "public"));
+
+// Set up static file serving
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
+
 
 app.get("/", (req, res) => {
-    res.send("Hello its from backend")
+    // res.send("Hello its from backend")
+    res.render("index")
 })
 
 io.on('connection', (socket) => {
